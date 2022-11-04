5ed2d5d39d85ca0d693e67c61c29658bedfe1f4e
RiffanNurArif <114058661+RiffanNurArif@users.noreply.github.com>
2022-11-04 10:57:07 +0700

    Kelompok3

---
+++ app/src/main/java/com/example/kelompok3intent/ResultActivity.java
@@ -0,0 +1,16 @@
+package com.example.kelompok3intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        //TODO
+        //1.Menampilkan detail pemenang pertandingan berdasarkan score tertinggi data berasal dari MatchActivity
+    }
+}
